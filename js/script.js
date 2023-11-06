a0dce2a8d3c9647f249360f8c13d4c0bd69b6c31
Lukasz Smiechowski <lukasz.smiechowski@me.com>
2023-11-06 17:40:40 +0100

    added js to coach section

--- js/script.js
+++ js/script.js
@@ -4,40 +4,46 @@ const navContainer = document.querySelector('.nav__container');
 const navMenuOpen = document.querySelector('.nav__mobile-menu');
 const navMenuClose = document.querySelector('.nav__mobile-close');
 
-const arturGrafik = document.querySelectorAll('.grafik__class-akro');
-const arturClass = document.querySelector('.grafik__class')
-const grafikContent = document.querySelectorAll('.grafik__plan-content');
+const arturGrafik = document.querySelectorAll('.coach__timetable-btn');
+const arturClass = document.querySelector('.coach__timetable-class')
+const grafikContent = document.querySelectorAll('.coach__schedule-content');
 
-const team = document.querySelector('.grafik__coach')
-const coach = document.querySelectorAll('.grafik__name');
+const team = document.querySelector('.coach__container')
+// const coach = document.querySelectorAll('.coach__btn');
 
+///
 navMenuOpen.addEventListener('click', function() {
     navContainer.classList.add('nav__open');
 })
 
-
+///
 navMenuClose.addEventListener('click', function() {
     navContainer.classList.remove('nav__open');
 })
 
-arturClass.addEventListener('click', function(e) {
-    const clicked = e.target.closest('.grafik__class-akro');
+///
+team.addEventListener('click', function(e) {
+    const clicked = e.target.closest('.coach__btn');
     // Guard clause
     if (!clicked) return;
 
-    arturGrafik.forEach(b => b.classList.remove('active'));
-    grafikContent.forEach(tab =>tab.classList.remove('grafik__plan-content-active'));
+    document.getElementById(`${clicked.dataset.tab}`).classList.toggle('coach__timetable-active');
+})
 
-    clicked.classList.add('active');
+///
+arturClass.addEventListener('click', function(e) {
+    const clicked = e.target.closest('.coach__timetable-btn');
+    // Guard clause
+    if (!clicked) return;
+
+    arturGrafik.forEach(b => b.classList.remove('coach__timetable-btn-active'));
+    grafikContent.forEach(tab =>tab.classList.remove('coach__schedule-content-active'));
+
+    clicked.classList.add('coach__timetable-btn-active');
 
     document
     .getElementById(`${clicked.dataset.tab}`)
-    .classList.add('grafik__plan-content-active');
+    .classList.add('coach__schedule-content-active');
 })
 
-team.addEventListener('click', function(e) {
-    const clicked = e.target.closest('.grafik__name');
-    console.log(clicked.dataset.tab);
 
-    document.getElementById(`${clicked.dataset.tab}`).classList.toggle('active');
-})
